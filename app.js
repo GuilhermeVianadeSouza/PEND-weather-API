0fea8ac18f09edd0cf0b9793eac87d1db52a95be
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-10-06 10:36:08 -0300

    Ajustando o cabeçario da melhor forma possivel, ainda necessario reconfigurações. Necessario ajustes em torno do css dessa área. Foi realizado o link de acesso a outras áreas

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 'use strict'
 
-const botaoVerificarTempo = document.getElementById('search')
-const usuario = document.getElementById('usuario')
-const notificacao = document.getElementById('notificacao')
-const redesSociais = document.querySelectorAll('.fa-brands')
+const botaoVerificarTempo = document.getElementById('search') //declarando o botão que futuramente será inserido no header após clicar em 2 das 3 opções;
+const usuario = document.getElementById('usuario')//declarando o icone do usuario
+const notificacao = document.getElementById('notificacao')//declarando o icone de notificação
+const redesSociais = document.querySelectorAll('.fa-brands')// buscando todos os icones que tenham em sua composição fa-brands
 
-function mostrarCabeçario(){
+function mostrarCabeçario(){ //Criando a primeira função de mostrar o cabeçario da tela
     const cabecario = document.getElementById('cabecario')
+
+    function acessarPaginas(){ //Essa função dentro da principal de mostrar o cabeçario vai realizar a criação dos elementos textos que com links redirecionaram a outra página
+        const navegacao = document.createElement('nav')
+
+        const linkGeneral = document.createElement('a')
+        linkGeneral.textContent = 'General Forecast'
+        linkGeneral.href = "./generalForecast.html"
+        const link7Days = document.createElement('a')
+        link7Days.textContent = '7 Days for week'
+        link7Days.href = "./7daysforweek.html"
+        const linkSupport = document.createElement('a')
+        linkSupport.textContent = 'Support'
+        linkSupport.href = "./support.html"
+        
+        navegacao.appendChild(linkGeneral)
+        navegacao.appendChild(link7Days)
+        navegacao.appendChild(linkSupport)
+
+        return navegacao
+    }
 
     const container = document.createElement('div')
     container.classList.add('notificacao')
@@ -16,11 +36,15 @@ function mostrarCabeçario(){
     logo.src = './img/logo.png'
     logo.alt = 'logo'
 
-    const botaoLogin = document.createElement('login')
+    const botaoLogin = document.createElement('button')
+    botaoLogin.addEventListener('click', () => {
+        alert('área em desenvolvimento')
+    })
 
 
     container.appendChild(botaoLogin)
     container.appendChild(logo)
+    cabecario.appendChild(acessarPaginas())
     cabecario.appendChild(container)
 }
 
